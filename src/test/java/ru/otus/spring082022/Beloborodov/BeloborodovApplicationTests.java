1afe1604331a63fe6fd520c5e1877776d3308194
Ivan Beloborodov <ibeloborodov@mail.ru>
2022-09-01 13:03:53 +0300

    #1

---
+++ src/test/java/ru/otus/spring082022/Beloborodov/BeloborodovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring082022.Beloborodov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeloborodovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
