43de36b3c2b059027898b41d893929bdc1694386
Ivan Beloborodov <ibeloborodov@mail.ru>
2022-09-01 13:03:53 +0300

    #1

---
+++ src/main/java/ru/otus/spring082022/Beloborodov/BeloborodovApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spring082022.Beloborodov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeloborodovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeloborodovApplication.class, args);
+	}
+
+}
