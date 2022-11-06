70c366a57447d48b808d92783084d147b4dc1354
IvanV8 <69732408+IvanV8@users.noreply.github.com>
2022-11-06 23:06:03 +0300

    Homework 08

--- Homework 08/src/main/java/ru/otus/spring082022/homework08/repositories/CustomRepositoryComment.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring082022.homework08.repositories;
-
-import ru.otus.spring082022.homework08.domain.Comment;
-
-import java.util.List;
-
-public interface CustomRepositoryComment {
-    List<Comment> findAllByBookId(String bookId);
-}
