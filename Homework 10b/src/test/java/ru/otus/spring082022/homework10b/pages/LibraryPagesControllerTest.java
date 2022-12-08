135f306be0daad7ca6ec3ea1c35c8869a346b406
IvanV8 <69732408+IvanV8@users.noreply.github.com>
2022-12-08 22:29:48 +0300

    Homework 10

---
+++ Homework 10b/src/test/java/ru/otus/spring082022/homework10b/pages/LibraryPagesControllerTest.java
@@ -0,0 +1,71 @@
+package ru.otus.spring082022.homework10b.pages;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.spring082022.homework10b.Homework10bApplication;
+import ru.otus.spring082022.homework10b.domain.Author;
+import ru.otus.spring082022.homework10b.domain.Genre;
+import ru.otus.spring082022.homework10b.dto.BookDto;
+import ru.otus.spring082022.homework10b.dto.CommentDto;
+import ru.otus.spring082022.homework10b.service.LibraryService;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ContextConfiguration(classes = Homework10bApplication.class)
+@WebMvcTest(controllers = LibraryPagesController.class)
+public class LibraryPagesControllerTest {
+
+    private static final long EXISTING_ID = 1L;
+    private static final long EXISTING_BOOK_AUTHOR_ID = 1;
+    private static final String EXISTING_BOOK_AUTHOR_NAME = "Alexander Pushkin";
+    private static final long EXISTING_BOOK_GENRE_ID = 1;
+    private static final String EXISTING_BOOK_GENRE_NAME = "Roman";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private LibraryService libraryService;
+
+    @Test
+    @DisplayName("API сохраняет книгу")
+    public void shouldSaveBook() throws Exception {
+        BookDto book = new BookDto(-1, "Title", "ISBN",
+                new Author(EXISTING_BOOK_AUTHOR_ID, EXISTING_BOOK_AUTHOR_NAME),
+                new Genre(EXISTING_BOOK_GENRE_ID, EXISTING_BOOK_GENRE_NAME));
+
+        mockMvc.perform(
+                        post("/save-book").contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(book)))
+                .andExpect(status().is(302));
+
+    }
+
+    @Test
+    @DisplayName("API сохраняет коммент")
+    public void shouldSaveComment() throws Exception {
+
+        CommentDto comment = new CommentDto(-1, "user", LocalDateTime.now(), "comment", 1);
+
+        mockMvc.perform(
+                        post("/save-comment/" + EXISTING_ID).contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(comment)))
+                .andExpect(status().is(302));
+
+    }
+
+}
